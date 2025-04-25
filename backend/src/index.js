f225ca0c7ca208a41cf0572fbb759f4727519959
nam <29ngtrung@gmail.com>
2025-04-26 02:03:04 +0700

    Db setup + user model setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv"
 
+import { connectDB } from "./lib/db.js";
+
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
 import authRoutes from "./routes/auth.route.js";
@@ -23,4 +25,5 @@ app.use("/api/stats", statRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} !`)
+    connectDB();
 })
