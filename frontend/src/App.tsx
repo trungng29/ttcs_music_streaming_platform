18d323719687c13f4735c3a54ed9879200652537
nam <29ngtrung@gmail.com>
2025-04-26 00:58:37 +0700

    Frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-500 text-5xl">Hello World</h1>
+      <Button variant={"outline"}> This is a button </Button>
+    </>
+  )
+}
+
+export default App
