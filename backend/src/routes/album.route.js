3bda4407633d3ba7a438c666233a80bdf35ba9a9
nam <29ngtrung@gmail.com>
2025-04-26 10:31:12 +0700

    Album Routes & Controllers

--- backend/src/routes/album.route.js
+++ backend/src/routes/album.route.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
+import { getAlbumById, getAllAlbums } from "../controller/album.controller.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send("Album route with GET method !");
-})
+router.get("/", getAllAlbums);
+router.get("/:albumId", getAlbumById);
 
-export default router
+export default router;
