c412d34a7cf14a92aa8b03ce607292a607995ced
nam <29ngtrung@gmail.com>
2025-05-17 19:43:54 +0700

    Playback controls component

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -12,7 +12,7 @@ const Topbar = () => {
     <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75 backdrop-blur-md z-10">
       <div className="flex gap-2 items-center">
         <img src="/spotify.png" alt="Spotify logo" className="size-8" />
-        Fotispy
+        Spotify
       </div>
 
       <div className="flex items-center gap-4">
--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import LeftSidebar from "./components/LeftSidebar";
 import FriendsActivity from "./components/FriendsActivity";
 import AudioPlayer from "./components/AudioPlayer";
+import { PlaybackControls } from "./components/PlaybackControls";
 
 const MainLayout = () => {
     const isMobile = false;
@@ -30,6 +31,7 @@ const MainLayout = () => {
         </ResizablePanel>
         
       </ResizablePanelGroup>
+      <PlaybackControls />
     </div>
   );
 };
