266939346b94d8e1b594c991a78c179cacca3231
nam <29ngtrung@gmail.com>
2025-05-26 13:56:43 +0700

    Done API LIke bai hat

--- backend/src/routes/song.route.js
+++ backend/src/routes/song.route.js
@@ -6,11 +6,11 @@ import { getLikedSongsOfUser } from "../controller/user.controller.js";
 const router = Router();
 
 router.get("/", protectRoute, requireAdmin, getAllSongs);
-router.get("/featured", protectRoute, getFeaturedSongs);
-router.get("/made-for-you", protectRoute, getMadeForYouSongs);
-router.get("/trending", protectRoute, getTrendingSongs);
-router.get("/search", protectRoute, getSongsByTitle);
-router.get("/:id", protectRoute, getSongById);
+router.get("/featured", getFeaturedSongs);
+router.get("/made-for-you", getMadeForYouSongs);
+router.get("/trending", getTrendingSongs);
+router.get("/search", getSongsByTitle);
+router.get("/:id", getSongById);
 router.get("/:userId/liked-songs", protectRoute, getLikedSongsOfUser);
 
 export default router;
